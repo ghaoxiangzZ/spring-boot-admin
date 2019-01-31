0d81f83d7a307c07ac4916b42d07edb5b8f5a21e
ghaoxiang <784390929@qq.com>
2019-01-31 10:53:11 +0800

    spring-boot-admin集成邮件预警

---
+++ admin-client/src/main/java/com/ghaoxiang/client/AdminClientApplication.java
@@ -0,0 +1,16 @@
+package com.ghaoxiang.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminClientApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/ghaoxiang/admin/SpringBootAdminApplication.java
@@ -0,0 +1,14 @@
+package com.ghaoxiang.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
+
